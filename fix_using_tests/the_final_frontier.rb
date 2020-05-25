d34011c9f4c43768bb2f324b1414113145bb4a00
Giulia Cajati <giuliacajati@gmail.com>
2020-05-25 15:10:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,13 +5,6 @@ def generate_star_date
   return star_date 
 end
 
-def state_log(star_date)
- "Captain's Log, star date #{star_date}."
-end
-
 def engage(star_date)
    "Captain's Log, star date #{star_date}."
-  
-  puts state_log(date)
-  date = generate_star_date
 end
